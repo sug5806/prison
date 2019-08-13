d6097fee28b0c41ae77068bee6cc8758e948d474
sug5806 <sug5806@gmail.com>
2019-08-13 21:47:37 +0900

    refac: add if statement to show_media in view.py

--- room/views.py
+++ room/views.py
@@ -25,22 +25,23 @@ def pre_save(sender, instance, **kwagrs):
 
 
 def show_media(request):
-    page = int(request.GET.get('page', 1))
-    paginated_by = 5
-    rooms = Media.objects.all()
+    if request.method == "GET" or request.is_ajax():
+        page = int(request.GET.get('page', 1))
+        paginated_by = 5
+        rooms = Media.objects.all()
 
-    total_count = len(rooms)
-    start_index = paginated_by * (page - 1)
-    end_index = paginated_by * page
-    rooms = rooms[start_index:end_index]
-    rng = range(1, math.ceil(total_count / paginated_by) + 1)
+        total_count = len(rooms)
+        start_index = paginated_by * (page - 1)
+        end_index = paginated_by * page
+        rooms = rooms[start_index:end_index]
+        rng = range(1, math.ceil(total_count / paginated_by) + 1)
 
-    context = {
-        'object_list': rooms,
-        'range': rng,
-    }
+        context = {
+            'object_list': rooms,
+            'range': rng,
+        }
 
-    return render(request, 'room/media_list.html', context)
+        return render(request, 'room/media_list.html', context)
 
 
 def search(request):
